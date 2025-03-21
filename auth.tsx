de1ce0142a2a3843c86d71836c64d2e00d081d5f
xDipzz <deepakmahajan.2004@gmail.com>
2025-03-21 17:22:06 +0530

    working on auth

--- auth.tsx
+++ auth.tsx
@@ -5,6 +5,8 @@ import { StackServerApp } from "@stackframe/stack";
 export const stackServerApp = new StackServerApp({
   tokenStore: "nextjs-cookie",
   urls: {
-    afterSignIn: "/dashboard",
-  }
+    afterSignIn: "", 
+    afterSignOut: "", 
+  },
 });
+console.log("StackServerApp Methods:", Object.keys(stackServerApp));
