847a8e857105b0d388e0cefb2b2d5db373b8924f
xDipzz <deepakmahajan.2004@gmail.com>
2025-03-24 01:42:08 +0530

    nextauth js adding for auth

---
+++ src/app/api/auth/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user.id = token.id;
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+  },
+});
--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -28,7 +28,7 @@ export default async function IndexPage() {
               rel="noreferrer"
               className="font-medium text-blue-600 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300 transition-colors underline"
             >
-              Deepak Mahajan
+              Dipz
             </a>
           </>
         }
